7ffedd247c7cdd830200a4920d5f4156b34c0a34
Mahantesh Kadavar <mahanteshk9845@gmail.com>
2025-12-15 15:12:53 +0530

    Add comments to Resilience4j configuration

--- loan-service/src/main/java/com/microservice/service/LoanService.java
+++ loan-service/src/main/java/com/microservice/service/LoanService.java
@@ -13,7 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
